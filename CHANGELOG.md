f455a17207f8da53d8f910031d35f010dbd8e75d
N0ps32 <github@raeder.technology>
2019-02-02 03:46:46 +0100

    replace host url in link header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this p
 ## 1.0.0 - 2019-02-02
 ### Added
 - Initial release
+
+## 1.0.1 - 2019-02-02
+### Added
+- Replace host base url for better compatibility
--- src/Http2ServerPushTwig.php
+++ src/Http2ServerPushTwig.php
@@ -14,7 +14,7 @@ class Http2ServerPushTwig extends Plugin
 
     public static $plugin;
 
-    public $schemaVersion = '1.0.0';
+    public $schemaVersion = '1.0.1';
 
     public function init()
     {
@@ -30,10 +30,12 @@ class Http2ServerPushTwig extends Plugin
 
         Event::on(View::class, View::EVENT_AFTER_RENDER_PAGE_TEMPLATE, function () {
             $assets = [];
+            $hostUrl = preg_replace("/\/?$/", '', Craft::$app->request->getAbsoluteUrl());
 
             //iterate over assets to build Link substrings
             foreach (Http2ServerPushTwigExtension::$assetsToPush as $asset => $config) {
                 $type = $config['type'];
+                $asset = str_replace($hostUrl, '', $asset);
                 if ($config['crossorigin']) {
                     $assets[] = "<$asset>; rel=preload; as=$type; crossorigin";
                 } else {
